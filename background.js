c5061d88ca8769752deb9e341c6d6ad472ce98d7
Thomas <latiosthinh@live.com>
2021-07-23 20:55:02 +0700

    add motphim + block jw ads

--- background.js
+++ background.js
@@ -6,16 +6,31 @@ chrome.runtime.onInstalled.addListener(() => {
 } );
 
 async function focusView() {
-	const video = document.querySelector( '.film-view iframe' )
+	const targets = [ '.film-view iframe', '#player iframe' ]
 	const head = document.head || document.getElementsByTagName('head')[0]
-	const focusClass = '<style id="focus-style">.focusView{ position:fixed;top:0;left:0;z-index:9999999999999 }</style>'
+	const focusClass = `
+		<style id="focus-style">
+			.focusView{ position:fixed !important;top:0 !important;left:0 !important;z-index:9999999999999 !important }
+			.div-control, #catfish{ display: none !important }
+		</style>`
 
 	await chrome.storage.sync.get( 'fv', ( {fv} ) => {
 		if ( fv === true ) {
 			head.insertAdjacentHTML( 'beforeend', focusClass );
-			video.classList.add( 'focusView' )
+			targets.forEach( t => {
+				const el = document.querySelector( t )
+				if ( el ) {
+					console.log(el)
+					el.classList.add( 'focusView' )
+				}
+			} )
 		} else {
-			video.classList.remove( 'focusView' )
+			targets.forEach( t => {
+				const el = document.querySelector( t )
+				if ( el ) {
+					el.classList.remove( 'focusView' )
+				}
+			} )
 			document.getElementById( 'focus-style' ).remove()
 		}
 	} );
